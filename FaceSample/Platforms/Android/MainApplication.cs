b929df3a073c2b198df4e98a190fcb4a884f395f
Pavel Masiuk <masukpasha@gmail.com>
2023-04-07 13:04:57 +0300

    initial commit

---
+++ FaceSample/Platforms/Android/MainApplication.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Runtime;
+using FaceSample.Platforms.Android;
+
+namespace FaceSample;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+        DependencyService.Register<IFaceSdkInit, FaceSdkInit>();
+        DependencyService.Register<IFaceSdk, FaceSdk>();
+        DependencyService.Register<IPhotoPickerService, PhotoPickerService>();
+    }
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
